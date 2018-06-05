fbc2fc707cce136e2e8a6bdf4d7d42e84bf78d4a
ruiXueYanFengFei <wuruiyz@163.com>
2018-06-05 12:30:03 +0800

    新增一些demo

---
+++ rspDetection/src/main/java/com/iursp/conf/DruidConfiguration.java
@@ -0,0 +1,49 @@
+package com.iursp.conf;
+
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+
+@Configuration
+public class DruidConfiguration {
+	
+	private Logger logger = LoggerFactory.getLogger(DruidConfiguration.class);
+	
+	@Bean
+	@ConfigurationProperties("spring.datasource")
+	public DataSource druidDataSource(){
+		DruidDataSource dataSource = new DruidDataSource();
+		return dataSource;
+	}
+	
+	@Bean
+	public ServletRegistrationBean druidStatViewServlet() {
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+			registrationBean.addInitParameter("allow", "127.0.0.1");// IP白名单 (没有配置或者为空，则允许所有访问)
+			registrationBean.addInitParameter("deny", "");// IP黑名单 (存在共同时，deny优先于allow)
+			registrationBean.addInitParameter("loginUsername", "root");
+			registrationBean.addInitParameter("loginPassword", "root");
+			registrationBean.addInitParameter("resetEnable", "false");
+		return registrationBean;
+	}
+
+	@Bean
+	public FilterRegistrationBean druidWebStatViewFilter() {
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean(new WebStatFilter());
+			registrationBean.addInitParameter("urlPatterns", "/*");
+			registrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*");
+		return registrationBean;
+	}
+}
+
